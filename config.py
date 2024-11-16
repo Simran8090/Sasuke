8a1381546f3ed9b10cd0cba0cd185699bb377b77
Simran8090 <simrandixit8090@gmail.com>
2024-11-16 19:26:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VIPMODXOWNER
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7847068102:AAH5MhODBHwfrzeQE5fpgTtw_-nThttbE7U")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@VIPMODXOWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7519482969")
